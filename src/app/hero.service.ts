027d35184213221a88d274963a8c95e1da66bee1
Parisa <parisa.khataei@georgebrown.ca>
2019-10-01 10:32:48 -0400

    update

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heros';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -9,11 +9,13 @@ import { MessageService } from './message.service';
 })
 
 export class HeroService {
+  constructor(private messageService: MessageService) { }
+  
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
+  
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  constructor(private messageService: MessageService) { }
+  
 
 }
